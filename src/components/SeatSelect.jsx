a3bfabd7cfce4aeb00f6b34b3e55f48798f3c6df
JeffMota <jmota906@gmail.com>
2022-12-11 14:09:25 -0300

    Seat selection functional

--- src/components/SeatSelect.jsx
+++ src/components/SeatSelect.jsx
@@ -6,10 +6,19 @@ import FooterSession from "./FooterSession"
 
 export default function SeatSelect() {
     const [sessionData, setSessionData] = useState([])
+    const [selected, setSelected] = useState([])
 
     const [loaded, setLoaded] = useState(false)
 
     const { idSessao } = useParams()
+
+    function select(id) {
+        let aux = [...selected, id]
+        if (selected.includes(id)) {
+            aux = aux.filter(elm => elm !== id)
+        }
+        setSelected(aux)
+    }
 
     useEffect(() => {
 
@@ -29,11 +38,12 @@ export default function SeatSelect() {
             {(loaded) &&
                 <SeatList>
                     {sessionData.seats.map(seat =>
-                        <Seat 
-                            key={seat.id} 
-                            disabled={seat.isAvailable ? false : true} 
-                            available={seat.isAvailable}>
-                                    {(seat.name.length == 1) ? '0' + seat.name : seat.name}
+                        <Seat
+                            onClick={() => select(seat.id)}
+                            key={seat.id}
+                            disabled={seat.isAvailable ? false : true}
+                            available={(selected.includes(seat.id)) ? 'selected' : seat.isAvailable}>
+                            {(seat.name.length == 1) ? '0' + seat.name : seat.name}
                         </Seat>
                     )}
 
@@ -48,11 +58,33 @@ export default function SeatSelect() {
                 />
             }
 
-            <div>
-                <Seat available={'selected'}></Seat>
-                <Seat available={true}></Seat>
-                <Seat available={false}></Seat>
-            </div>
+            <Description>
+                <div>
+                    <Seat available={'selected'}></Seat>
+                    <p>Selecionado</p>
+                </div>
+                <div>
+                    <Seat available={true}></Seat>
+                    <p>Disponível</p>
+                </div>
+                <div>
+                    <Seat available={false}></Seat>
+                    <p>Indisponível</p>
+                </div>
+            </Description>
+
+            <Form>
+                <label>
+                    Nome do comprador: 
+                    <input type="text" placeholder="Digite o seu nome..."/>
+                </label>
+                <label>
+                    CPF do comprador: 
+                    <input type="text" placeholder="Digite o seu nome..."/>
+                </label>
+
+                <button type="submit">Reservar assento(s)</button>
+            </Form>
         </SeatContainer>
     )
 }
@@ -78,7 +110,6 @@ const SeatContainer = styled.div`
         color: #293845;
     }
 `
-
 const SeatList = styled.div`
     display: grid;
     grid-template-columns: repeat(10, 1fr);
@@ -88,7 +119,6 @@ const SeatList = styled.div`
     width: 85%;
     
 `
-
 const Seat = styled.button`
     width: 26px;
     height: 26px;
@@ -97,8 +127,32 @@ const Seat = styled.button`
 
     border: 1px solid ${props => (props.available) ? (props.available == true ? '#808F9D' : '#0E7D71') : '#F7C52B'};
     border-radius: 50%;
-    background-color: ${props => (props.available) ? (props.available == true ? '#C3CFD9' : '#1AAE9E')  : '#FBE192'};
+    background-color: ${props => (props.available) ? (props.available == true ? '#C3CFD9' : '#1AAE9E') : '#FBE192'};
 
     font-size: 11px;
     font-family: 'Roboto', sans-serif;
+`
+const Description = styled.div`
+    display: flex;
+
+    margin-top: 20px;
+    justify-content: space-between;
+
+    width: 75%;
+
+    > div{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+
+        font-size: 13px;
+        color: #4E5A65;
+    }
+`
+const Form = styled.form`
+    > label{
+        display:flex;
+        flex-direction: column;
+    }
 `
