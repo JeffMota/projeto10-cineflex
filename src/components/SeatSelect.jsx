6722a059d75a5a6f98b08a959d19699e5a210e1b
JeffMota <jmota906@gmail.com>
2022-12-11 12:28:58 -0300

    Seat select list

--- src/components/SeatSelect.jsx
+++ src/components/SeatSelect.jsx
@@ -1,15 +1,15 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import styled from "styled-components"
 import FooterSession from "./FooterSession"
 
-export default function SeatSelect(){
+export default function SeatSelect() {
     const [sessionData, setSessionData] = useState([])
 
     const [loaded, setLoaded] = useState(false)
 
-    const {idSessao} = useParams()
+    const { idSessao } = useParams()
 
     useEffect(() => {
 
@@ -17,21 +17,42 @@ export default function SeatSelect(){
         promise.then(res => {
             setSessionData(res.data)
             setLoaded(true)
-            console.log(res.data)
         })
         promise.catch(err => console.log(err))
 
     }, [])
 
-    return(
+    return (
         <SeatContainer>
             <p>Selecione o(s) assento(s)</p>
-            {(loaded) && 
-                <FooterSession 
-                    title={sessionData.movie.title} 
-                    poster={sessionData.movie.posterURL} 
+
+            {(loaded) &&
+                <SeatList>
+                    {sessionData.seats.map(seat =>
+                        <Seat 
+                            key={seat.id} 
+                            disabled={seat.isAvailable ? false : true} 
+                            available={seat.isAvailable}>
+                                    {(seat.name.length == 1) ? '0' + seat.name : seat.name}
+                        </Seat>
+                    )}
+
+                </SeatList>
+            }
+
+            {(loaded) &&
+                <FooterSession
+                    title={sessionData.movie.title}
+                    poster={sessionData.movie.posterURL}
                     day={`${sessionData.day.weekday} - ${sessionData.day.date}`}
-                />}
+                />
+            }
+
+            <div>
+                <Seat available={'selected'}></Seat>
+                <Seat available={true}></Seat>
+                <Seat available={false}></Seat>
+            </div>
         </SeatContainer>
     )
 }
@@ -56,4 +77,28 @@ const SeatContainer = styled.div`
         font-size: 24px;
         color: #293845;
     }
+`
+
+const SeatList = styled.div`
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+
+    gap: 15px 7px;
+
+    width: 85%;
+    
+`
+
+const Seat = styled.button`
+    width: 26px;
+    height: 26px;
+
+    color: black;
+
+    border: 1px solid ${props => (props.available) ? (props.available == true ? '#808F9D' : '#0E7D71') : '#F7C52B'};
+    border-radius: 50%;
+    background-color: ${props => (props.available) ? (props.available == true ? '#C3CFD9' : '#1AAE9E')  : '#FBE192'};
+
+    font-size: 11px;
+    font-family: 'Roboto', sans-serif;
 `
